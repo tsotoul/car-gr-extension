896ff5cb6aa2bf982684a0c621b973b0bc514365
tsotoul <tsotoul@hotmail.com>
2024-10-29 18:49:29 +0000

    added delay

--- background.js
+++ background.js
@@ -5,33 +5,39 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.sync.get("trackedListings", (data) => {
       const listings = data.trackedListings || [];
       let notificationCount = 0;
-      listings.forEach((listing, index) => {
-        chrome.tabs.create({ url: listing.url, active: false }, (tab) => {
-          chrome.scripting.executeScript(
-            {
-              target: { tabId: tab.id },
-              func: () => {
-                var result = document.querySelector(".price-text").textContent.replace(/\./g, "");
-                return result;
-              },
-            },
-            (results) => {
-              const newPrice = parseFloat(results[0].result);
 
-              if (newPrice == listing.price) {
-                notificationCount++;
-                console.log("Price checked.")
-                chrome.action.setBadgeText({ text: notificationCount.toString() });
-                chrome.action.setBadgeBackgroundColor({ color: "#FF0000" });
-                if(newPrice) {
+      listings.forEach((listing, index) => {
+        setTimeout(() => { // Add delay with setTimeout
+          chrome.tabs.create({ url: listing.url, active: false }, (tab) => {
+            chrome.scripting.executeScript(
+              {
+                target: { tabId: tab.id },
+                func: () => {
+                  const priceElement = document.querySelector(".price-text");
+                  return priceElement ? priceElement.textContent.replace(/\./g, "") : null;
+                },
+              },
+              (results) => {
+                if (results[0] && results[0].result) {
+                  const newPrice = parseFloat(results[0].result);
+
+                  // Check if the price has changed
+                  if (newPrice === listing.price) {
+                    notificationCount++;
+                    chrome.action.setBadgeText({ text: notificationCount.toString() });
+                    chrome.action.setBadgeBackgroundColor({ color: "#FF0000" });
+                    
+                    // Update the listing with the new price
                     listings[index].price = newPrice;
+                    chrome.storage.sync.set({ trackedListings: listings });
+                  }
+                  console.log("Price checked for listing:", listing.url);
                 }
+                chrome.tabs.remove(tab.id);
               }
-              chrome.storage.sync.set({ trackedListings: listings });
-              chrome.tabs.remove(tab.id);
-            }
-          );
-        });
+            );
+          });
+        }, index * 10000); // Delay each request by 10 seconds * index
       });
     });
   }
